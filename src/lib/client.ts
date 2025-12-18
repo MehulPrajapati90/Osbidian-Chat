c460abadcd1dddfd56b246dbdacd2c0b9378742b
Mehul Prajapati <ailearn90@gmail.com>
2025-12-19 03:05:42 +0530

    added base-url for proper connection

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,4 +1,6 @@
 import { treaty } from "@elysiajs/eden"
 import type { App } from "../app/api/[[...slug]]/route"
 
-export const client = treaty<App>("http://localhost:3000").api;
+const url = process.env.BASE_URL || "http://localhost:3000"
+
+export const client = treaty<App>(url).api;

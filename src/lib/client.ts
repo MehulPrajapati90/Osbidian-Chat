6cefd9d66d7fd9b34fb0ecd8fddfd4e8c2422a28
Mehul Prajapati <ailearn90@gmail.com>
2025-12-19 03:22:56 +0530

    added fix in env name

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,6 +1,6 @@
 import { treaty } from "@elysiajs/eden"
 import type { App } from "../app/api/[[...slug]]/route"
 
-const url = process.env.BASE_URL || "http://localhost:3000"
+const url = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"
 
 export const client = treaty<App>(url).api;
